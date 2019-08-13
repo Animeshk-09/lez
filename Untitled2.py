65caab5901091be6c63f56fc83a4044d3ee91583
Animeshk-09 <53961299+Animeshk-09@users.noreply.github.com>
2019-08-13 23:08:13 +0530

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+'''
+Objective: Program to implement GUESS THE NUMBER game . 
+           The user is given 5 chances to guess a number in the range 10<=number<=50.
+           
+           If the user's guess matches the number generated , 
+           Python displays 'YOU WIN ' and the loop terminates without completing the five iteration.
+           
+           If however , cannot guess the number in five attempts, python displays 'YOU LOSE'.
+           
+Input Parameter: integer value between [10,50]
+return value : string
+'''
+
+
+import random
+number=random.randint(10,50)
+c=0
+while c<5:
+    guess = int(input("Guess a number in range 10..50:"))
+    if guess == number:
+        print("YOU WIN!!:)")
+        
+        break
+    else:
+        c+=1
+if not c<5:
+    print("YOU LOSE :(\n The number was", number)
+    
+
+
+# In[ ]:
+
+
+
+
